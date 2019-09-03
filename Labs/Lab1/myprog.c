1f6d8a7d0b737a0ab73d86398525583978fc719c
Kartikeya Sharma <ks061@bucknell.edu>
2019-09-03 12:51:54 -0400

    Pre-lab 1.3 completed

---
+++ Labs/Lab1/myprog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(void) {
+	int count = 1;
+	int child_one = fork();
+	int child_two = fork();
+	if (child_one > 0 && child_two > 0) {
+		while (1) {
+			printf("Parent: %d\n", count);
+			count++;
+		}
+	} else if (child_one == 0 && child_two > 0) {
+		while (1) {
+			printf("Child #1: %d\n", count);
+			count++;
+		}
+	} else if (child_one > 0 && child_two == 0) {
+		while (1) {
+			printf("Child #2: %d\n", count);
+			count++;
+		}
+	} else {
+	}
+	return 0;
+}
