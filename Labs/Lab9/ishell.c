a0eb1dcf4db0eb004622cc4a3455ce03f788c5cc
Kartikeya Sharma <ks061@bucknell.edu>
2019-11-19 10:17:22 -0500

    Lab 9.3 completed

--- Labs/Lab9/ishell.c
+++ Labs/Lab9/ishell.c
@@ -9,9 +9,11 @@
 
 int main(int argc, char *argv[]) {
 	char input_str[MAX_INPUT_SIZE];
+
+	char *cmd_token, *arg_token;
+	char *cmd_saveptr, *arg_saveptr;
+	char **cmd_args;
 	
-	char* token;
-	char** cmd_args = malloc(sizeof(char*) * MAX_NUM_ARGS);
 	int i;
 
 	int pid;
@@ -25,25 +27,34 @@ int main(int argc, char *argv[]) {
 		if (strcmp(input_str, "exit\n") == 0)
 			break; // break if exit command entered
 		if (input_str[strlen(input_str)-1] == '\n')
-			input_str[strlen(input_str)-1] = '\0';
-
-		token = strtok(input_str, " ");
-		i = 0;
-		while (token) {
-			cmd_args[i++] = token;
-			token = strtok(NULL, " ");
+			input_str[strlen(input_str)-1] = '\0'; // replace new line character
+							       // used in command line UI with
+							       // null terminator used to handle
+							       // strings in C
+		
+		cmd_token = strtok_r(input_str, ";", &cmd_saveptr);
+		while (cmd_token) {
+			arg_token = strtok_r(cmd_token, " ", &arg_saveptr);
+			i = 0;
+			cmd_args = malloc(sizeof(char*) * MAX_NUM_ARGS);
+			while (arg_token) {
+				cmd_args[i++] = arg_token;
+				arg_token = strtok_r(NULL, " ", &arg_saveptr);
+			}
+			cmd_args[i] = NULL;
+			pid = Fork();
+			if (pid == 0) {
+				Execvp(cmd_args[0], cmd_args);
+			} else {
+				Wait(&status);
+				termination_status = WEXITSTATUS(status);
+				if (termination_status > 0) printf("[ishell: program terminated abnormally][\"%d\"]\n", termination_status);
+				else printf("[ishell: program terminated successfully]\n");
+			}
+			free(cmd_args[i]);
+			cmd_token = strtok_r(NULL, ";", &cmd_saveptr);
 		}
-		cmd_args[i] = NULL;
-
-		pid = Fork();
-		if (pid == 0) {
-			Execvp(cmd_args[0], cmd_args);		
-		} else {
-			Wait(&status);
-			termination_status = WEXITSTATUS(status);
-			if (termination_status > 0) printf("[ishell: program terminated abnormally][\"%d\"]\n", termination_status);
-			else printf("[ishell: program terminated successfully]\n");
-		}
+		
 	}
 	return 0;
 }
